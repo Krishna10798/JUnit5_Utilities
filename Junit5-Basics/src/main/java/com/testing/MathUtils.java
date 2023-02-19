556f26ae07365cf38612aba0985dc4ea033ac58f
Krishna Yadav2 <KrishnaY2@citiustech.com>
2023-02-19 16:25:24 +0530

    1st commite uploaded for maths utils

---
+++ Junit5-Basics/src/main/java/com/testing/MathUtils.java
@@ -0,0 +1,22 @@
+package com.testing;
+
+public class MathUtils {
+
+     public int add(int a,int b) {
+    	 return a+b;
+     }
+     public int subtract(int a,int b) {
+    	 return a-b;
+     }
+     public int multiply(int a,int b) {
+    	 return a*b;
+     }
+     public int devide(int a,int b) {
+    	 return a/b;
+     }
+     
+     public double computeCircleArea(double radious) {
+    	 return Math.PI * radious *radious;
+     }
+
+}
